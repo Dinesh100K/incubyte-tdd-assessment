ec3ddef69f0eb9e4c6c620f9a50c50e28546ca09
Dinesh <dknayak2997@gmail.com>
2025-04-17 10:18:37 +0530

    refactor the code to move custom delimiter code to separate methods

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,15 +2,8 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
    
-    delimiters = /,|\n/
-    num_str = numbers
-
-    # Custom delimiter syntax: //[delim]\n[numbers]
-    if numbers.start_with?('//')
-      delimiter_spec, num_str = numbers.split("\n", 2)
-      custom = delimiter_spec[2..]
-      delimiters = Regexp.new(Regexp.escape(custom))
-    end
+    delimiters = determine_delimiters(numbers)
+    num_str = extract_numbers_string(numbers)
 
     nums = num_str.split(delimiters).map(&:to_i)
 
@@ -21,5 +14,25 @@ class StringCalculator
      end
  
     nums.sum
+  end
+
+  private
+
+  def determine_delimiters(numbers)
+    if numbers.start_with?('//')
+      delimiter_spec, = numbers.split("\n", 2)
+      custom = delimiter_spec[2..]
+      Regexp.new(Regexp.escape(custom))
+    else
+      /,|\n/
+    end
+  end
+
+  def extract_numbers_string(numbers)
+    if numbers.start_with?('//')
+      numbers.split("\n", 2).last
+    else
+      numbers
+    end
   end
 end
