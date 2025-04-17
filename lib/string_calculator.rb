b78f7c93d80eb682dba54fca43a2cab54ae25d8a
Dinesh <dknayak2997@gmail.com>
2025-04-17 06:05:33 +0530

    update the add method to handle new lines between the numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    numbers = numbers.gsub("\n", ',')
     numbers_array = numbers.split(',').map(&:to_i)
     numbers_array.sum
   end
