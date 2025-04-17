22fd8574efd7ae2f803e9b193899cecab25fe360
Dinesh <dknayak2997@gmail.com>
2025-04-17 05:39:34 +0530

    add test case for single number input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it "should return 0 for the empty string" do
       expect(calc.add("")).to eq(0)
     end
+
+    it 'should return a number when single number input is present' do
+      expect(calc.add("5")).to eq(5)
+    end
   end
 end
